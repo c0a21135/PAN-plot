4a76c76a38aeeadcc4a6483f3daca45152525e4a
c0a21135 <c0a211351b@edu.teu.ac.jp>
2023-11-07 13:52:37 +0900

    bindPopup生成を変更

--- app/project/plot/WEB-INF/src/Create/CreatePlot.java
+++ app/project/plot/WEB-INF/src/Create/CreatePlot.java
@@ -1,8 +1,10 @@
 package Create;
 
 public class CreatePlot {
-    public static String Plot(String name, double locate_x, double locate_y){
+    public static String Plot(String name, double locate_x, double locate_y, int id){
         return 
-        "var marker = L.marker(["+locate_x+","+ locate_y + "]).addTo(mymap); marker.bindPopup('"+name+"');";
+        "var marker = L.marker(["+locate_x+","+ locate_y + "]).addTo(mymap);" +
+        "var popupContent = '<form action=\"/plot/ShopInfo\" method=\"post\" target=\"_blank\" rel=\"noopener noreferrer\" accept-charset=\"UTF-8\"><button name=\"shop_id\" value="+id+">"+name+"</form>';" +
+        "marker.bindPopup(popupContent);";
     }
 }
