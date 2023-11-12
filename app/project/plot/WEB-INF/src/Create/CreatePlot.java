0e95e963e7f0700d5ad8f7f1734d31f4614273cc
c0a21135 <c0a211351b@edu.teu.ac.jp>
2023-11-13 01:03:16 +0900

    店舗のマーカー色を黄色に変更

--- app/project/plot/WEB-INF/src/Create/CreatePlot.java
+++ app/project/plot/WEB-INF/src/Create/CreatePlot.java
@@ -3,7 +3,7 @@ package Create;
 public class CreatePlot {
     public static String Plot(String name, double locate_x, double locate_y, int id){
         return 
-        "var marker = L.marker(["+locate_x+","+ locate_y + "]).addTo(mymap);" +
+        "var marker = L.marker(["+locate_x+","+ locate_y + "], {icon: yellowIcon}).addTo(mymap);" +
         "var popupContent = '<form action=\"/plot/ShopInfo\" method=\"post\" target=\"_blank\" rel=\"noopener noreferrer\" accept-charset=\"UTF-8\"><button name=\"shop_id\" value="+id+">"+name+"</form>';" +
         "marker.bindPopup(popupContent);";
     }
